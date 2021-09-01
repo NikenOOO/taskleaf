67a4573bc94f2ef4ffbb36b6b906a69485c2f24f
NikenOOO <kouhei23.0927@gmail.com>
2021-09-01 14:24:17 +0900

    add sort and search

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,19 @@
-class Task < ApplicationRecord  
+class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate :validate_name_not_including_comma
-  
+
   belongs_to :user
-  
+
+  scope :recent, -> { order(created_at: :desc) }
+
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   private
 
   def validate_name_not_including_comma
